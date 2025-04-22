0b167074f450e54e359a7cdf9f01e9d37c70d1a7
Mayur-0409 <158018024+Mayur-0409@users.noreply.github.com>
2025-04-22 22:40:02 +0530

    Update DatabaseConfig.java

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -1,11 +1,1 @@
-package config;
 
-public class DatabaseConfig {
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/pharmsdb";
-    public static final String USER = "root";
-    public static final String PASS = "Mayur@1234";
-    
-    private DatabaseConfig() {
-        // Private constructor to prevent instantiation
-    }
-} 
